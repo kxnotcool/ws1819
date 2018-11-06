bea60639f6b4a7d914dc690f3ba845dea29300e3
yankunwu <wyk.kapp@live.com>
2018-11-06 23:35:46 +0100

    problem1 of cv1 assignment1 has been done

--- ws1819_/cv1/exercise/assignment1_data/problem1.jl
+++ ws1819_/cv1/exercise/assignment1_data/problem1.jl
@@ -1,31 +1,56 @@
 using PyPlot
 using FileIO
+using Images
+using JLD2
 
 # load and return the given image
 function loadimage()
-  img = load(File(format"PNG",alp1.png))
+  imgo = load("a1p1.png")
+  temp = channelview(convert(Array{RGB{Float32}},imgo))
+  img = convert(Array{Float32,3},temp)
   return img::Array{Float32,3}
+
 end
 
 # save the image as a .jld2 file
 function savefile(img::Array{Float32,3})
-
+  save("img.jld2","img",img)
 end
 
 # load and return the .jld2 file
 function loadfile()
-
+  img = load("img.jld2", "img")
   return img::Array{Float32,3}
 end
 
 # create and return a horizontally mirrored image
 function mirrorhorizontal(img::Array{Float32,3})
-
+  imgSize = size(img)
+  mirrored = Array{Float32,3}(undef, imgSize[1], imgSize[2], imgSize[3])
+  for i = 1:450
+      mirrored[:,:,i] = img[:,:,451-i]
+  end
   return mirrored::Array{Float32,3}
 end
 
 # display the normal and the mirrored image in one plot
 function showimages(img1::Array{Float32,3}, img2::Array{Float32,3})
+  imgSize1=size(img1)
+  imgSize2=size(img2)
+  imgshow1 = Array{Float32,3}(undef, imgSize1[2], imgSize1[3], 3)
+  imgshow2 = Array{Float32,3}(undef, imgSize2[2], imgSize2[3], 3)
+  for i=1:imgSize1[2]
+    for j=1:imgSize1[3]
+      imgshow1[i,j,:] = img1[:,i,j]
+      imgshow2[i,j,:] = img2[:,i,j]
+    end
+  end
+
+  PyPlot.subplot(211)
+  PyPlot.imshow(imgshow1)
+  PyPlot.subplot(212)
+  PyPlot.imshow(imgshow2)
+  gcf()
 
 end
 
