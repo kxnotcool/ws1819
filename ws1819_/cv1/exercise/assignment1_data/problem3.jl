2a48389b5c8874cca1f5f46566a157bbc920b1d8
yankunwu <wyk.kapp@live.com>
2018-11-09 16:20:18 +0100

    finish problem4 of cv1 first assignment

--- ws1819_/cv1/exercise/assignment1_data/problem3.jl
+++ ws1819_/cv1/exercise/assignment1_data/problem3.jl
@@ -6,6 +6,7 @@ using FileIO
 
 # Transform from Cartesian to homogeneous coordinates
 function cart2hom(points::Array{Float64,2})
+  points_hom = vcat(points, ones(1, size(points)[2]))
 
 
   return points_hom::Array{Float64,2}
@@ -14,6 +15,11 @@ end
 
 # Transform from homogeneous to Cartesian coordinates
 function hom2cart(points::Array{Float64,2})
+  points_cart = Array{Float64, 2}(undef,size(points)[1]-1,size(points)[2])
+
+  for i = 1:size(points)[2]
+    points_cart[:,i] = points[1:size(points)[1]-1,i]/points[size(points)[1],i]
+  end
 
   return points_cart::Array{Float64,2}
 end
@@ -21,39 +27,75 @@ end
 
 # Translation by v
 function gettranslation(v::Array{Float64,1})
+  T=Matrix{Float64}(I, 4, 4)
 
+
+  T[1,4] = v[1]
+  T[2,4] = v[2]
+  T[3,4] = v[3]
   return T::Array{Float64,2}
 end
 
 # Rotation of d degrees around x axis
 function getxrotation(d::Int)
-
+  a_angle = d/180 * pi
+  Rx = zeros(3,3)
+  Rx[1,1] = 1
+  Rx[2,2] = cos(a_angle)
+  Rx[2,3] = -sin(a_angle)
+  Rx[3,2] = sin(a_angle)
+  Rx[3,3] = cos(a_angle)
   return Rx::Array{Float64,2}
 end
 
 # Rotation of d degrees around y axis
 function getyrotation(d::Int)
-
+  a_angle = d/180 * pi
+  Ry = zeros(3,3)
+  Ry[1,1] = cos(a_angle)
+  Ry[2,2] = 1
+  Ry[1,3] = sin(a_angle)
+  Ry[3,1] = -sin(a_angle)
+  Ry[3,3] = cos(a_angle)
   return Ry::Array{Float64,2}
 end
 
 # Rotation of d degrees around z axis
 function getzrotation(d::Int)
-
+  a_angle = d/180 * pi
+  Rz = zeros(3,3)
+  Rz[1,1] = cos(a_angle)
+  Rz[2,2] = cos(a_angle)
+  Rz[1,2] = -sin(a_angle)
+  Rz[2,1] = sin(a_angle)
+  Rz[3,3] = 1
   return Rz::Array{Float64,2}
 end
 
 
 # Central projection matrix (including camera intrinsics)
 function getcentralprojection(principal::Array{Int,1}, focal::Int)
-
+  K = Matrix{Float64}(I,3,3)
+  K[1,1] = convert(Float64, focal)
+  K[2,2] = convert(Float64, focal)
+  K[1,3] = convert(Float64, principal[1])
+  K[2,3] = convert(Float64, principal[2])
+  #temp1 = hcat(Matrix{Int}(I,3,3), zeros(3,1))
+  K = hcat(K, zeros(3,1))
   return K::Array{Float64,2}
 end
 
 
 # Return full projection matrix P and full model transformation matrix M
 function getfullprojection(T::Array{Float64,2},Rx::Array{Float64,2},Ry::Array{Float64,2},Rz::Array{Float64,2},V::Array{Float64,2})
-
+  R = Rz * Rx * Ry
+  R = hcat(R, zeros(3,1))
+  R = vcat(R, zeros(1,4))
+  R[4,4] = 1
+  M = R * T
+  P = V * M
+  #M = vcat(M, zeros(1, size(M)[2]))
+  #M[size(M)[1],size(M)[2]] = 1
   return P::Array{Float64,2},M::Array{Float64,2}
 end
 
@@ -61,6 +103,7 @@ end
 
 # Load 2D points
 function loadpoints()
+  points = load("obj2d.jld2", "x")
 
   return points::Array{Float64,2}
 end
@@ -68,20 +111,26 @@ end
 
 # Load z-coordinates
 function loadz()
-
+  z = load("zs.jld2", "Z")
   return z::Array{Float64,2}
 end
 
 
 # Invert just the central projection P of 2d points *P2d* with z-coordinates *z*
 function invertprojection(P::Array{Float64,2}, P2d::Array{Float64,2}, z::Array{Float64,2})
+  P2d = vcat(P2d, z)
+  P3d = P\P2d
+  P3d[4,:] =ones(1, size(P3d)[2])
+
 
   return P3d::Array{Float64,2}
 end
 
-
 # Invert just the model transformation of the 3D points *P3d*
 function inverttransformation(A::Array{Float64,2}, P3d::Array{Float64,2})
+
+  X = A\P3d
+
 
   return X::Array{Float64,2}
 end
@@ -89,20 +138,29 @@ end
 
 # Plot 2D points
 function displaypoints2d(points::Array{Float64,2})
-
+  PyPlot.plot(points[1,:], points[2,:])
   return gcf()::Figure
 end
 
 # Plot 3D points
 function displaypoints3d(points::Array{Float64,2})
-
+  points_x = points[1,:]
+  points_y = points[2,:]
+  points_z = points[3,:]
+  PyPlot.scatter3D(points_x, points_y, points_z)
   return gcf()::Figure
 end
 
 # Apply full projection matrix *C* to 3D points *X*
 function projectpoints(P::Array{Float64,2}, X::Array{Float64,2})
-
-  return P2d:::Array{Float64,2}
+  temp = vcat(X, ones(1, size(X)[2]))
+  P3d = Array{Float64, 2}(undef, 3, size(temp)[2])
+  for i = 1 : size(temp)[2]
+    P3d[:,i] = P*temp[:,i]
+  end
+  P2d = P3d[1:2, :]
+  #P2d=hom2cart(P2d)
+  return P2d::Array{Float64,2}
 end
 
 
@@ -147,3 +205,13 @@ function problem3()
   @test points ≈ points2
   return
 end
+
+
+#why it is necessary to provide z-coordinates?
+# From the perspective of the matrix calculation, we need the z-coordinates to
+#calculates the x and y-coordinates of Xi.
+# From the perspective of geometrry, our camera center is not at the principal axis,
+#so we need z-coordinates to calculates the offset.
+#Are the Rotations commutative?
+#No, i previously used the wrong order, that results to a wrong plot. Because the
+#multiplications of matrix are not commutative. 
--- ws1819_/cv1/exercise/assignment1_data/problem4.jl
+++ ws1819_/cv1/exercise/assignment1_data/problem4.jl
@@ -3,12 +3,31 @@ using PyPlot
 
 # Create 3x3 derivative filters in x and y direction
 function createfilters()
+  fx = ones(3,3)
+  fy = ones(3,3)
+  fx_unit = Array{Float64,2}(undef, 1,3)
+  g_unit = Array{Float64,2}(undef, 3,1)
+  fx_unit[1,1] = 1/2
+  fx_unit[1,2] = 0
+  fx_unit[1,3] = -1/2
+
+  Gsy1 = 1/sqrt(2*pi)/0.9 * exp(-1/2/0.9^2)
+  Gsy0 = 1/sqrt(2*pi)/0.9
+  g_unit[1,1] = Gsy1
+  g_unit[2,1] = Gsy0
+  g_unit[3,1] = Gsy1
+  fx = g_unit * fx_unit
+
+  fy = fx_unit' * g_unit'   #normalization?
+
 
   return fx::Array{Float64,2}, fy::Array{Float64,2}
 end
 
 # Apply derivate filters to an image and return the derivative images
 function filterimage(I::Array{Float32,2},fx::Array{Float64,2},fy::Array{Float64,2})
+  Ix = imfilter(I, fx)
+  Iy = imfilter(I, fy)
 
 
   return Ix::Array{Float64,2},Iy::Array{Float64,2}
@@ -17,6 +36,17 @@ end
 
 # Apply thresholding on the gradient magnitudes to detect edges
 function detectedges(Ix::Array{Float64,2},Iy::Array{Float64,2}, thr::Float64)
+  Ix_s = broadcast(*, Ix,Ix)
+  Iy_s = broadcast(*, Iy,Iy)
+  edges = broadcast(sqrt, Ix_s + Iy_s)
+  for i = 1 : size(edges)[1]
+    for j = 1 : size(edges)[2]
+      if edges[i,j] < thr
+
+        edges[i,j] = 0
+      end
+    end
+  end
 
   return edges::Array{Float64,2}
 end
@@ -24,6 +54,44 @@ end
 
 # Apply non-maximum-suppression
 function nonmaxsupp(edges::Array{Float64,2},Ix::Array{Float64,2},Iy::Array{Float64,2})
+  row_size = size(Ix)[1]
+  col_size = size(Ix)[2]
+  Ix_m = broadcast(sqrt, Ix.*Ix)
+  Iy_m = broadcast(sqrt, Iy.*Iy)
+
+  Iy_mp = vcat(zeros(1, col_size), Iy_m)
+  Iy_mp = vcat(Iy_mp, zeros(1, col_size))
+  Iy_mp = hcat(zeros(row_size + 2, 1), Iy_mp)
+  Iy_mp = hcat(Iy_mp, zeros(row_size + 2, 1))
+
+  Ix_mp = vcat(zeros(1, col_size), Ix_m)
+  Ix_mp = vcat(Ix_mp, zeros(1, col_size))
+  Ix_mp = hcat(zeros(row_size + 2, 1), Ix_mp)
+  Ix_mp = hcat(Ix_mp, zeros(row_size + 2, 1))
+
+  edges_p = vcat(zeros(1, col_size), edges)
+  edges_p = vcat(edges_p, zeros(1, col_size))
+  edges_p = hcat(zeros(row_size + 2, 1), edges_p)
+  edges_p = hcat(edges_p, zeros(row_size + 2, 1))
+
+  for i = 2 : size(edges_p)[1] - 1
+    for j =  2 : size(edges_p)[2] - 1
+      if edges_p[i,j] == 0
+        continue
+      end
+
+      if abs(edges_p[i-1,j] - edges_p[i+1,j]) <= abs(edges_p[i,j-1] - edges_p[i,j+1])
+        if Ix_mp[i-1,j] > Ix_mp[i,j] || Ix_mp[i+1,j] > Ix_mp[i,j]
+          edges[i-1,j-1] = 0
+        end
+      else
+        if Iy_mp[i,j-1] > Iy_mp[i,j] || Iy_mp[i,j+1] > Iy_mp[i,j]
+          edges[i-1,j-1] = 0
+        end
+      end
+
+    end
+  end
 
   return edges::Array{Float64,2}
 end
@@ -63,7 +131,7 @@ function problem4()
   gcf()
 
   # threshold derivative
-  threshold = 38. / 255.
+  threshold = 18. / 255.
   edges = detectedges(imgx,imgy,threshold)
   figure()
   imshow(edges.>0, "gray", interpolation="none")
@@ -80,3 +148,6 @@ function problem4()
   gcf()
   return
 end
+# f = (1,2,3)   h = (10,20,30)
+#     (4,5,6)       (40,50,60)
+#     (7,8,9)       (70,80,90)
